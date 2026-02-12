9762073d25d019f0ae5d33b07e6854c425b2cd0f
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-02-12 20:01:47 +0900

    wip: wip

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ fn main() {
     let z = mul(x, y);
     println!("z = {z}");
 
-    example01()
+    example01();
+    example02();
 }
 
 fn mul(x: i32, y: i32) -> i32 {
@@ -15,4 +16,12 @@ fn example01() {
     let mut n: u64 = 100;
     let a: &u64 = &n;
     println!("*a = {}, addr = {:p}", *a, a);
+}
+
+fn example02() {
+    let arr: [u32; 4] = [1, 2, 3, 4];
+    println!("{}, {}, {}, {}", arr[0], arr[1], arr[2], arr[3]);
+
+    let s: &[u32] = &arr[1..3];
+    println!("{:?}", s);
 }

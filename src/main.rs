890386569c847f71288e507f7e895157e7f28033
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-03-01 15:43:56 +0900

    wip: wip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 use std::collections::LinkedList;
 
 fn main() {
@@ -364,6 +365,37 @@ fn example05() {
 
     if let Some(value) = m.get(&3) {
         println!("{}", value);
+    }
+
+    let mut s = BTreeSet::new();
+    s.insert(100);
+    s.insert(400);
+    s.insert(6);
+    s.insert(1);
+
+    for n in s.iter() {
+        println!("{n}");
+    }
+
+    let mut v = Vec::new();
+    v.push(10);
+    v.push(5);
+
+    let mut s = BTreeSet::new();
+    s.insert(100);
+    s.insert(400);
+
+    let it = v.iter().chain(s.iter());
+    for n in it.clone().map(|n| n * 2) {
+        println!("{n}");
+    }
+
+    let total = it.clone().fold(0, |acc, x| acc + x);
+
+    let list: LinkedList<_> = it.filter(|n| *n % 2 == 0).collect();
+
+    for (n, m) in v.iter().zip(s.iter()) {
+        println!("({n}, {m}");
     }
 }
 

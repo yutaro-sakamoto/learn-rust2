69e80c1f576681204710a2ea54148299f60289e0
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-03-03 20:20:47 +0900

    wip: wip

--- src/main.rs
+++ src/main.rs
@@ -501,6 +501,30 @@ fn example05() {
 
     //single_threaded();
     //multi_threaded();
+
+    struct H2O {}
+    struct O2 {}
+    struct H2 {}
+
+    fn burn(_h2_1: H2, _h2_2: H2, _o2: O2) -> (H2O, H2O) {
+        (H2O {}, H2O {})
+    }
+
+    let h2_1 = H2 {};
+    let h2_2 = H2 {};
+    let o2 = O2 {};
+    let (h2o_1, h2o_2) = burn(h2_1, h2_2, o2);
+
+    struct Coin {}
+
+    let a = Coin {};
+    let b = a;
+    let c = b;
+
+    let a = 10;
+    let b = 20;
+    let c = a + b;
+    let d = a * b;
 }
 
 //fn example06() {

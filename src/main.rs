197b8990b45557cd385bf4ae327d5ec610594cc5
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-03-27 20:59:54 +0900

    wip: wip

--- src/main.rs
+++ src/main.rs
@@ -906,6 +906,38 @@ fn example05() {
         //let b1 = b::b_1::TypeB1;
         let b2 = b::b_2::TypeB2;
     }
+
+    mod bb {
+        pub struct TypeB;
+        mod b_1 {
+            pub struct TypeB1 {
+                pub n: usize,
+                m: usize,
+            }
+
+            impl TypeB1 {
+                fn g(&self) {}
+                pub fn h(&self) {}
+            }
+
+            fn f1(p: &super::b_1::TypeB1) {
+                println!("{}", p.n);
+                println!("{}", p.m);
+                p.g();
+            }
+        }
+
+        mod b_2 {
+            pub struct TypeB2;
+
+            fn f2(p: &super::b_1::TypeB1) {
+                println!("{}", p.n);
+                //println!("{}", p.m);
+                //p.g();
+                p.h();
+            }
+        }
+    }
 }
 fn run_rw_lock_example() {
     let mut gallery = BTreeMap::new();
